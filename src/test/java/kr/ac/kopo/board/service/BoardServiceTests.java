2d2305058bb574c8a1a6bd381350d3ae5142c0e9
AI510-165 <2201110362@office.kopo.ac.kr>
2023-09-07 16:22:38 +0900

    20230907

---
+++ src/test/java/kr/ac/kopo/board/service/BoardServiceTests.java
@@ -0,0 +1,39 @@
+package kr.ac.kopo.board.service;
+
+import kr.ac.kopo.board.dto.BoardDTO;
+import kr.ac.kopo.board.dto.PageRequestDTO;
+import kr.ac.kopo.board.dto.PageResultDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardServiceTests {
+
+    @Autowired
+    private BoardService boardService;
+
+    @Test
+    public void testRegister() {
+
+        BoardDTO dto = BoardDTO.builder()
+                .title("New Test title")
+                .content("New Test content")
+                .writerEmail("user1@kopo.ac.kr")  // 현재 데이터베이스에 존재하는 회원 이메일
+                .build();
+
+        Long bno = boardService.register(dto);
+    }
+
+    @Test
+    public void testList() {
+        PageRequestDTO pageRequestDTO = new PageRequestDTO();
+
+        PageResultDTO<BoardDTO, Object[]> result = boardService.getList(pageRequestDTO);
+
+        for (BoardDTO boardDTO : result.getDtoList()) {
+            System.out.println(boardDTO);
+        }
+    }
+
+}
