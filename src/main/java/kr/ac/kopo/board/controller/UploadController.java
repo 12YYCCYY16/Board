6e0f431d5711eb8f28ce3b78a1ea6333d5d6ee62
AI510-165 <2201110362@office.kopo.ac.kr>
2023-10-12 15:26:31 +0900

    20231012 파일업로드

--- src/main/java/kr/ac/kopo/board/controller/UploadController.java
+++ src/main/java/kr/ac/kopo/board/controller/UploadController.java
@@ -1,32 +1,112 @@
 package kr.ac.kopo.board.controller;
 
+import kr.ac.kopo.board.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @Log4j2
 public class UploadController {
+
     @Value("${kr.ac.kopo.upload.path}")
     private String uploadPath;
+
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles) {
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) {
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
         for (MultipartFile uploadFile : uploadFiles) {
+
+            if (uploadFile.getContentType().startsWith("image")) {
+                log.warn("이미지 파일이 아닙니다.");
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            }
+
             String originalName = uploadFile.getOriginalFilename();
             String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
+
             log.info("파일명: " + fileName);
-            Path savePath = Paths.get(uploadPath + File.separator+fileName);
-            try{
+
+            // 날짜 폴더 생성
+            String folderPath = makeFolder();
+
+            // UUID 생성
+            String uuid = UUID.randomUUID().toString();
+            // 업로드파일의 이름이 중복되서 기존파일이 덮어쓰기되는것을 방지하기위해 랜덤한 uuid값 을 파일명에 추가
+            Path savePath = Paths.get(uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName);
+            try {
                 uploadFile.transferTo(savePath);
-            }catch (IOException e){
+                resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
+            } catch (IOException e) {
+
+            }
         }
+        return new ResponseEntity<>(resultDTOList,HttpStatus.OK);
     }
+
+        private String makeFolder () {
+            // 현재 날짜를 이용하여 폴더 경로 생성 (예: "2023/10/12")
+            String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+            // 경로에서 "//"을 File.separator로 대체하여 사용
+            String folderPath = str.replace("//", File.separator);
+
+            // 폴더가 존재하지 않는 경우 생성
+            File uploadPathFolder = new File(uploadPath, folderPath);
+            if (!uploadPathFolder.exists()) {
+                uploadPathFolder.mkdirs();
+            }
+
+            return folderPath;
+        }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+
+        ResponseEntity<byte[]> result = null;
+
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+
+            log.info("fileName: " + srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            log.info("file: " + file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            // MIME 타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+
+            // 파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
+    }
+
 }
-}
+
